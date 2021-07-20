b1b42be1552006b527a4412d89d8b8f06d5351cf
GabrielCremm <gcscremm@hotmail.com>
2021-07-20 14:37:40 -0300

    arrumando codigo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ button.addEventListener("click", function() {
     let typedText = document.getElementById("textInput").value;
     typedText = typedText.toLowerCase();
     typedText = typedText.replace(/[^a-z'\s]+/g, "");
-
+    words = typedText.split(/\s/);
     const letterCounts = {};
     for (let i = 0; i <= typedText.length; i++) {
         let currentLetter = typedText[i];
@@ -20,35 +20,36 @@ button.addEventListener("click", function() {
     for (let letter in letterCounts) {
         const span = document.createElement("span");
         const textContent = `${letter}: ${letterCounts[letter]} `;
+
         span.innerText = textContent;
         const letters = document.getElementById("lettersDiv");
         letters.appendChild(span);
     }
-
-    const ordenArray = letterCounts[letter].sort(function(first, second) {
-        //first = {meredith}
-        // second = {alex}
-
-        // comparar o campo desejado do first com o do second
-        // se for "Maior", então joga pra baixo
-        // se for "Menor", então joga pra cima
-        // se for "Igual", então não faz nada
-
-        const firstField = first[field]
-
-        const secondField = second[field]
-
-
-
-        if (firstField > secondField) {
-            return 1;
-        } else if (firstField < secondField) {
-            return -1;
+    const wordCounts = {};
+    for (let i = 0; i < words.length; i++) {
+        let currentWord = words[i];
+        if (wordCounts[currentWord] === undefined) {
+            wordCounts[currentWord] = 1;
         } else {
-            return 0;
+            wordCounts[currentWord]++;
         }
-    })
+    }
 
-    console.log(letterCounts);
+    for (let word in wordCounts) {
+        const span = document.createElement("span");
+        const textContent = `${word}: ${wordCounts[word]} `;
+
+        span.innerText = textContent;
+        const words2 = document.getElementById("lettersDiv");
+        words2.appendChild(span);
+    }
+
+
+
+
+
+    console.log(words);
+
+
 
 });
